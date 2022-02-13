8fdc6967eb419157cf943acbab272d6d57923192
Сергей Дмитриевич <58300647+Aznorb-Team@users.noreply.github.com>
2022-02-13 22:33:24 +0800

    Update MinMax.cpp

--- MinMax.cpp
+++ MinMax.cpp
@@ -12,32 +12,6 @@ int main()
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-#pragma omp parallel for
-    for (int i = 0; i < n; i++) {
-        if (max < a[i]) {
-            max = a[i];
-        }
-        if (min > a[i]) {
-            min = a[i];
-        }
-    }
-    cout << max << endl;
-    cout << min << endl;
-}
-#include <iostream>
-#include <omp.h>
-
-using namespace std;
-
-int main()
-{
-    int n;
-    cin >> n;
-    int* a = new int[n];
-    int max = INT16_MIN, min = INT16_MAX;
-    for (int i = 0; i < n; i++) {
-        cin >> a[i];
-    }
 
     double start;
     double end;
