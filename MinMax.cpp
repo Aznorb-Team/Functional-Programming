ca5587779c2429ba2f34b1c33b3d10256dcf6746
Сергей Дмитриевич <58300647+Aznorb-Team@users.noreply.github.com>
2022-02-13 21:28:47 +0800

    Update MinMax.cpp

--- MinMax.cpp
+++ MinMax.cpp
@@ -16,9 +16,6 @@ int main()
         if (max < a[i]) {
             max = a[i];
         }
-    }
-#pragma omp parallel for
-    for (int i = 0; i < n; i++) {
         if (min > a[i]) {
             min = a[i];
         }
