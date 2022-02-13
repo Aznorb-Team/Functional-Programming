68cb00ae82fcf328b4b7f227cb9bc30475eb4748
Сергей Дмитриевич <58300647+Aznorb-Team@users.noreply.github.com>
2022-02-13 21:26:33 +0800

    Add files via upload

---
+++ MinMax.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int* a = new int[n];
+    int max = INT16_MIN, min = INT16_MAX;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+#pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        if (max < a[i]) {
+            max = a[i];
+            cout << "Max update!" << endl;
+        }
+    }
+#pragma omp parallel for
+    for (int i = 0; i < n; i++) {
+        if (min > a[i]) {
+            min = a[i];
+            cout << "Min update!" << endl;
+        }
+    }
+    cout << max << endl;
+    cout << min << endl;
+}
