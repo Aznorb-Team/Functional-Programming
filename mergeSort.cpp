26c5162ce84238e717282921c31c31bd58290224
Сергей Дмитриевич <58300647+Aznorb-Team@users.noreply.github.com>
2022-02-17 00:39:15 +0800

    Update mergeSort.cpp

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -1,67 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
 #include <iostream>
+#define SIZE 16
 
-void merge(int* AP, int p, int q, int r) {
-    int n1 = q - p + 1;
-    int n2 = r - q;
-    int* L; int* R;
-    L = new int[n1 + 1];
-    R = new int[n2 + 1];
-#pragma omp parallel for
-    for (int i = 1;i <= n1; i++) {
-        L[i - 1] = AP[p + i - 1];
-    }
-#pragma omp parallel for
-    for (int j = 1;j <= n2; j++) {
-        R[j - 1] = AP[q + j];
-    }
-#pragma omp parallel for
-    for (int i = 0, j = 0, k = p; k <= r; k++) {
-        if (i < n1 && j < n2) {
-            if (L[i] <= R[j]) {
-                AP[k] = L[i];
-                i++;
-            }
-            else {
-                AP[k] = R[j];
-                j++;
-            }
-        }
-        else if (i < n1) {
-            AP[k] = L[i];
+using namespace std;
+// Функция сортировки нисходящим слиянием
+void mergeSort(int* a, int l, int r)
+{
+    if (l == r) return; // границы сомкнулись
+    int mid = (l + r) / 2; // определяем середину последовательности
+    // и рекурсивно вызываем функцию сортировки для каждой половины
+    mergeSort(a, l, mid);
+    mergeSort(a, mid + 1, r);
+    int i = l;  // начало первого пути
+    int j = mid + 1; // начало второго пути
+    int* tmp = (int*)malloc(r * sizeof(int)); // дополнительный массив
+//#pragma omp parallel for
+    for (int step = 0; step < r - l + 1; step++) // для всех элементов дополнительного массива
+    {
+        // записываем в формируемую последовательность меньший из элементов двух путей
+        // или остаток первого пути если j > r
+        if ((j > r) || ((i <= mid) && (a[i] < a[j])))
+        {
+            tmp[step] = a[i];
             i++;
         }
-        else {
-            AP[k] = R[j];
+        else
+        {
+            tmp[step] = a[j];
             j++;
         }
     }
-    delete[] L; delete[] R;
+    // переписываем сформированную последовательность в исходный массив
+    for (int step = 0; step < r - l + 1; step++)
+        a[l + step] = tmp[step];
 }
-void merge_sort(int* AP, int p, int r) {
-    int q;
-    if (p < r) {
-        q = (p + r) / 2;
-        merge_sort(AP, p, q);
-        merge_sort(AP, q + 1, r);
-        merge(AP, p, q, r);
+int main()
+{
+    int a[SIZE];
+    // Заполняем элементы массива
+    for (int i = 0; i < SIZE; i++)
+    {
+        a[i] = (rand() % 100);
+        printf(" %d ", a[i]);
     }
-}
-
-
-void print(int* A, int size) {
-#pragma omp parallel for
-    for (int i = 0; i < size; i++)
-        printf("%d ", A[i]);
+    double start;
+    double end;
+    start = omp_get_wtime();
+    mergeSort(a, 0, SIZE - 1); // вызываем функцию сортировки
     printf("\n");
-}
-
-int main() {
-    //int arr[] = { 4,2,7,1,23,1,23,1 };
-    int arr[] = { 3,6,4,1,5,7,2,9,8,10,3,11,14,12,15,19,18,20,17,13,11 };
-
-    print(arr, sizeof(arr) / sizeof(int));
-    merge_sort(arr, 0, sizeof(arr) / sizeof(int) - 1); printf("\n");
-    print(arr, sizeof(arr) / sizeof(int));
-
+    end = omp_get_wtime();
+    cout << "Work took %f seconds: " << end - start << endl;
+    // Выводим отсортированный массив
+    for (int i = 0; i < SIZE; i++)
+        printf(" %d ", a[i]);
     getchar();
+    return 0;
 }
