cc6bd8697579410a184450ea8c224fd8c86b36db
Сергей Дмитриевич <58300647+Aznorb-Team@users.noreply.github.com>
2022-03-14 10:00:58 +0800

    Update lab2.cpp

--- lab2.cpp
+++ lab2.cpp
@@ -13,16 +13,16 @@ void adjecencyMatrix(bool* visited)
     //Матрица смежности
     AdjacencyMatrix.resize(n, vector<int>(n));
 
-    cout << "Матрица смежности: " << endl;
+    //cout << "Матрица смежности: " << endl;
     for (int f = 0; f < n; f++)
     {
         visited[f] = false;
         for (int g = 0; g < n; g++)
         {
             AdjacencyMatrix[f][g] = 0 + rand() % 2;
-            cout << AdjacencyMatrix[f][g] << ' ';
+            //cout << AdjacencyMatrix[f][g] << ' ';
         }
-        cout << "\n";
+        //cout << "\n";
     }
 }
 
@@ -36,6 +36,20 @@ void DFS(int st, bool* visited)
         if ((AdjacencyMatrix[st][r] != 0) && (!visited[r]))
         {
             DFS(r, visited);
+        }
+    }
+}
+void DFSOmp(int st, bool* visited)
+{
+    int r;
+    cout << st + 1 << " ";
+    visited[st] = true;
+#pragma omp parallel for
+    for (r = 0; r < n; r++)
+    {
+        if ((AdjacencyMatrix[st][r] != 0) && (!visited[r]))
+        {
+            DFSOmp(r, visited);
         }
     }
 }
@@ -60,22 +74,49 @@ void BFS(int s) {
         }
     }
 }
+void BFSOmp(int s) {
+    queue <int> turn;
+    vector<bool> used(n);
+    used[s] = true;
+    turn.push(s);
+#pragma omp parallel for
+    for (; !turn.empty();) {
+        int ind = turn.front();//берем из очереди крайний элемент
+        turn.pop();//удаляем его
+        cout << ind + 1 << ' ';
+        for (int i = 0; i < AdjacencyMatrix[ind].size(); i++)//смотрим, с какими вершинами смежна вершина ind
+        {
+            if (AdjacencyMatrix[ind][i] == 1 && used[i] != true)
+            {
+                used[i] = true;
+                turn.push(i);//добавляем в очередь вершину i
 
+            }
+        }
+    }
+    //while (!turn.empty()) //проверяем, пуста ли очередь
+    //{
+        
+    //}
+}
 int main()
 {
     setlocale(LC_ALL, "rus");
     //Поиск в глубину
-    n = 5;
+    n = 1000;
     bool* visited = new bool[n];
     bool* visited1 = visited;
     adjecencyMatrix(visited);
     int start;
     start = 1;
 
-    cout << "Порядок обхода: ";
-    DFS(start - 1, visited);
-    cout << endl;
+    //cout << "Порядок обхода: ";
+   // DFS(start - 1, visited);
+    //cout << endl;
     cout << "Порядок обхода: ";
     BFS(start-1);
+    cout << endl;
+    cout << "Порядок обхода: ";
+    BFSOmp(start-1);
     return 0;
 }
